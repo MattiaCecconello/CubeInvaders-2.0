54ab12efc000eb6bc5d54539ffed935984b67b2f
redd096 <martin.babino96@gmail.com>
2021-04-29 15:00:10 +0200

    inserito level button graphics

---
+++ Assets/Scripts/LevelButtonGraphics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Cube Invaders/Level Button Graphics")]
+public class LevelButtonGraphics : MonoBehaviour
+{
+    [Header("Level Button")]
+    [SerializeField] GameObject noDamageObject = default;
+
+    public void SetNoDamage(bool active)
+    {
+        //active or deactive No Damage object
+        if(noDamageObject)
+        {
+            noDamageObject.SetActive(active);
+        }
+    }
+}
