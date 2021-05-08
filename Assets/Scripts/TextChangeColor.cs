48d961fcc1b8507e703cd5cd37cf49ed1c57ec9d
redd096 <martin.babino96@gmail.com>
2021-05-08 10:36:25 +0200

    cambia colore text per pad

---
+++ Assets/Scripts/TextChangeColor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[AddComponentMenu("Cube Invaders/Text Change Color")]
+public class TextChangeColor : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    [SerializeField] Text text;
+    [SerializeField] Color selectedColor = Color.black;
+
+    Color normalColor = Color.white;
+
+    public void Awake()
+    {
+        //get reference to Text Component
+        if (text == null)
+        {
+            text = GetComponent<Text>();
+        }
+
+        //get normalColor
+        if (text)
+        {
+            normalColor = text.color;
+        }
+    }
+
+    public void OnEnable()
+    {
+        //by default use normalColor
+        if (text)
+        {
+            text.color = normalColor;
+        }
+    }
+
+    public void OnDisable()
+    {
+        //reset to normalColor
+        if (text)
+        {
+            text.color = normalColor;
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        //use hover color
+        if (text)
+        {
+            text.color = selectedColor;
+        }
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        //reset to normal color
+        if (text)
+        {
+            text.color = normalColor;
+        }
+    }
+}
